2443809d88e032ad55038d7533769d164744671b
tyakovenko <taisia.yakovenko@gmail.com>
2024-02-06 16:34:12 -0500

    fns to draw bar chart and add dots on the first and second bars

--- barChart/barChartFns.js
+++ barChart/barChartFns.js
@@ -1,4 +1,5 @@
-// Set up the SVG width and height
+//https://d3-graph-gallery.com/barplot
+
 var svgWidth = 600;
 var svgHeight = 400;
 
@@ -15,42 +16,71 @@ var svg = d3.select("#chart-container")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Load data from CSV file
-d3.csv("array_2.csv").then(function(data) {
-    data.forEach(function(d) {
-        d.value = +d.value; // Convert string to number
+
+function findMiddleIndex (array){
+    arr = []
+    id1 = 0
+    id2 = 1
+    arr.append(id1, id2)
+    return arr
+}
+
+function loadOneFile (fileNum){
+    fileStr = "array_" + fileNum + ".csv";
+    // Load data from CSV file
+    d3.csv(fileStr).then(function(data) {
+        data.forEach(function(d) {
+            d.value = +d.value; // Convert string to number
+        });
+
+        // Define the scale for x-axis
+        var x = d3.scaleBand()
+            .domain(data.map(function(d) { return d.label; }))
+            .range([0, width])
+            .padding(0.1);
+
+        // Define the scale for y-axis
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return d.value; })])
+            .nice()
+            .range([height, 0]);
+
+        // Add x-axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add y-axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // Add bars
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.label); })
+            .attr("width", x.bandwidth())
+            .attr("y", function(d) { return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); });
+
+        // Filter data for dots at the bottom of the first and second bars
+        var dotsData = [
+            { label: data[0].label, value: data[0].value },
+            { label: data[1].label, value: data[1].value }
+        ];
+
+        // Add dots at the bottom of the first and second bars
+        svg.selectAll(".dot")
+            .data(dotsData)
+            .enter().append("circle")
+            .attr("class", "dot")
+            .attr("cx", function(d) { return x(d.label) + x.bandwidth() / 2; }) // middle of the bar
+            .attr("cy", function(d) { return height; }) // at the bottom of the chart
+            .attr("r", 5)
+            .style("fill", "red");
+
+    }).catch(function(error) {
+        console.log(error);
     });
-
-    // Define the scale for x-axis
-    var x = d3.scaleBand()
-        .domain(data.map(function(d) { return d.label; }))
-        .range([0, width])
-        .padding(0.1);
-
-    // Define the scale for y-axis
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return d.value; })])
-        .nice()
-        .range([height, 0]);
-
-    // Add x-axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add y-axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // Add bars
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.label); })
-        .attr("width", x.bandwidth())
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return height - y(d.value); });
-}).catch(function(error) {
-    console.log(error);
-});
+}
